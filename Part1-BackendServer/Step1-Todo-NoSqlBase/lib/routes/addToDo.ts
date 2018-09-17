4c62eb3ee71a032dfc6d143e8feba2bd28dced47
Naveed-rana <rana.naveed812@gmail.com>
2018-09-17 23:21:51 +0500

    Done with Part1-step1 restApi with NoSql DB and TDD pattern

--- Part1-BackendServer/Step1-Todo-NoSqlBase/lib/routes/addToDo.ts
+++ Part1-BackendServer/Step1-Todo-NoSqlBase/lib/routes/addToDo.ts
@@ -1,10 +1,86 @@
 import * as express from 'express';
-import todoModels from '../models/usersSchema';
+import todoModels from '../models/todoSchema';
 var router = express.Router();
 
-//check route is working
+//check todo route is working
 router.get('/',(req,res)=>{
     res.status(200).json("AddTodo working!")
+})
+
+//todo post restfull api
+
+router.post('/tasks',(req,res)=>{
+    let newTodo = new todoModels(req.body);
+    newTodo.save((err,todo)=>{
+        if(err){
+            res.status(500).json("error has been occored!");
+        }
+        else{
+            res.status(200).json(todo);
+        }
+    });
+});
+
+
+//get request restfull api
+router.get('/tasks',(req,res)=>{
+    todoModels.find({},(err,data)=>{
+        if(err){
+            res.status(401).json(err);
+        }
+        else{
+            res.status(200).json(data);
+        }
+    });
+    
+})
+
+
+//get one request restfull api
+router.get('/tasks:id',(req,res)=>{
+    
+    todoModels.findOne({_id:req.params.id},(err,data)=>{
+        if(err){
+        res.status(401).json(err);
+        }
+        else{
+            res.status(200).json(data);
+        }    
+    });   
+})
+
+
+//put request restfull api
+router.put('/tasks:id',(req,res)=>{
+
+    todoModels.findOneAndUpdate({_id: req.params.id},
+         {$set:{
+             title:req.body.title,
+             discription:req.body.discription,
+             done:req.body.done
+        }},
+         (err,data)=>{
+        if(err){
+        res.status(401).json(err);
+       }
+        else{
+        res.status(200).json(data);
+        }    
+    });   
+})
+
+
+//delete request restfull api
+router.delete('/tasks:id',(req,res)=>{
+
+    todoModels.findOneAndDelete({_id:req.params.id},(err)=>{
+        if(err){
+            res.status(500).json("error has been occored!");
+        }
+        else{
+            res.status(200).json("deleted");
+        }
+    });
 })
 
 
