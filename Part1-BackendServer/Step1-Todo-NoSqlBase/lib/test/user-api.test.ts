78ddcaf56354a842ce72e3eb25873c9b62b681a3
Naveed-rana <rana.naveed812@gmail.com>
2018-09-17 19:09:22 +0500

    write test for signup route and its fail first time

--- Part1-BackendServer/Step1-Todo-NoSqlBase/lib/test/user-api.test.ts
+++ Part1-BackendServer/Step1-Todo-NoSqlBase/lib/test/user-api.test.ts
@@ -1,9 +1,27 @@
 import * as request from "supertest";
 import {App} from '../app';
 
+
+//test /user route is working or not
 describe("GET /Api", () => {
   it("should return 200 OK", () => {
     return request(App).get("/user")
       .expect(200);
   });
 });
+
+//test post req in user route
+
+describe('POST /users', function() {
+  it('responds with json', function(done) {
+    request(App)
+      .post('/user/signup')
+      .send({name: 'naveed',email:'rana.naveed812@gmail.com',password:'124'})
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        done();
+      });
+  });
+});
+
