7f3c9316990f9115f45f4030495566e63f1fa742
Naveed-rana <rana.naveed812@gmail.com>
2018-09-17 19:49:58 +0500

    pass the route and refactor the code according to TDD

--- Part1-BackendServer/Step1-Todo-NoSqlBase/lib/routes/users.ts
+++ Part1-BackendServer/Step1-Todo-NoSqlBase/lib/routes/users.ts
@@ -3,12 +3,24 @@ import * as passport from 'passport';
 import usersModels from '../models/usersSchema';
 var router = express.Router();
 
-
+//check route is working
 router.get('/',(req,res)=>{
     res.status(200).json("route working!")
 })
 
+//route for signup
 
+router.post('/signup',(req,res)=>{
+    let newUser = new usersModels(req.body);
+    newUser.save((err,user)=>{
+        if(err){
+            res.status(500).json("error has been occored!")
+        }
+        else{
+            res.status(200).json("Account has been created!")
+        }
+    });
+});
 
 
 export default router;
