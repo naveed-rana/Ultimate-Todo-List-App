258e200a75cef470a3fafcac46cbc11a9248401e
Naveed-rana <rana.naveed812@gmail.com>
2018-09-18 14:11:57 +0500

    add login test fail due to route not found

--- Part1-BackendServer/Step2-Todo-SqlDB/lib/test/user-api.test.ts
+++ Part1-BackendServer/Step2-Todo-SqlDB/lib/test/user-api.test.ts
@@ -10,21 +10,34 @@ describe("GET /Api", () => {
   });
 });
 
-//test post signup req in user route
+// //test post signup req in user route
 
-describe('POST /users', function() {
+// describe('POST /users', function() {
 
+//   it('responds with json', function(done) {
+//     request(App)
+//       .post('/user/signup')
+//       .send({name: 'Ali',email:'rana.naveed684@gmail.com',password:'124'})
+//       .expect(200)
+//       .end(function(err, res) {
+//         if (err) return done(err);
+//         done();
+//       });
+//   });
+
+// });
+
+
+//test post Login req in user route using sqldb
+describe('POST /users/login using sqldb', function() {
   it('responds with json', function(done) {
     request(App)
-      .post('/user/signup')
-      .send({name: 'Ali',email:'rana.naveed684@gmail.com',password:'124'})
+      .post('/user/login')
+      .send({email:'rana.naveed812@gmail.com',password:'124'})
       .expect(200)
       .end(function(err, res) {
         if (err) return done(err);
         done();
       });
   });
-
 });
-
-
