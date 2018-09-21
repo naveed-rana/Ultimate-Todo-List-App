b3c6f8ea48a83ebe77318664efb35e7761b6282f
Asif-chohan <rmasifchohan@gmail.com>
2018-09-20 23:28:04 -0700

    Debug conflict

--- Part2-ClientApp/Step1-Design-Ui/src/Components/Redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import AddTodoReducers from './AddTodoReducers';
-
-const rootReducer = combineReducers({
-        AddTodo:AddTodoReducers,
-});
-
-export default rootReducer;
