eeb74a14fc9c9ee850c77fbdfea07a6454cdded3
abaidmalik243 <abaidmalik243@gmail.com>
2018-09-22 20:44:01 +0500

    Update todo successfully done from indexedDB

--- Part2-ClientApp/Step3-TodoIndexedDB-App/src/Components/Redux/actions/UpdateToDoActions/index.js
+++ Part2-ClientApp/Step3-TodoIndexedDB-App/src/Components/Redux/actions/UpdateToDoActions/index.js
@@ -1,10 +1,30 @@
+import db from '../../../config/db';
+
 export const UPDATE_TODO = 'UPDATE_TODO';
 
+// export function updateToDo(data) {
+//     // alert("alert from UPDATE action");
+//     // console.log('Update actiton data: ', data)
+//     return {
+//         type: UPDATE_TODO,
+//         payload: data
+//     }
+// }
+
+
 export function updateToDo(data) {
-    // alert("alert from UPDATE action");
-    // console.log('Update actiton data: ', data)
-    return {
-        type: UPDATE_TODO,
-        payload: data
-    }
+    // alert('Update Action func');
+    // console.log('Check Dexie id is: ', data.id);
+    // console.log('Check Dexie data: ', data);
+
+    return (dispatch) => {
+        db.table('todos')
+            .update(data.id, { title: data.title, desc: data.desc })
+            .then(() => {
+                  dispatch({
+                    type: UPDATE_TODO,
+                    payload: data
+                });
+            });
+    };
 }
