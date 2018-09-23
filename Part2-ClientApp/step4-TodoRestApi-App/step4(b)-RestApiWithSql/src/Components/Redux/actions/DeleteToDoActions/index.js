475762b47ae6fb6b6e042cf3ed861444e0de0edf
zahid786 <zahidali8208@gmail.com>
2018-09-23 19:03:17 +0500

    successfully handle delete requist with sql

---
+++ Part2-ClientApp/step4-TodoRestApi-App/step4(b)-RestApiWithSql/src/Components/Redux/actions/DeleteToDoActions/index.js
@@ -0,0 +1,27 @@
+import { toast } from 'react-toastify';
+import axios from 'axios';
+
+export const REMOVE_TODO = 'REMOVE_TODO';
+
+//Remove data from indexedDB
+export function removeToDo(id) {
+    
+    console.log(id);
+    
+    return (dispatch) => {
+        axios.delete(window.baseURL + `/todo/api/v1.0/tasks/${id}`)
+            .then(() => {
+                toast.success("Successfully deleted!");
+
+                dispatch({
+                    //Call Reducer
+                    type: REMOVE_TODO,
+                    payload: id
+                });
+            }).catch(()=>{
+                toast.error("Error Occored! Please Try Again Later");
+
+
+            })
+    };
+}
